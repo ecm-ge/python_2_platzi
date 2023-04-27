f9ef2c74d004287097efed0f11cf6e62989dfd2f
Golden Eagle <econtrerasm@unbosque.edu.co>
2023-04-27 15:38:18 -0500

    Agregando Data extra

--- web-server/main.py
+++ web-server/main.py
@@ -1,4 +1,20 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def getList():
+    return [1,2,3,4]
+
+@app.get('/contact', response_class=HTMLResponse)
+def getList():
+    return """
+    
+    <h1>Hola soy una pagina</h1>
+    <p>Soy un parrafo</p>
+    """
 
 def run():
     store.getCategories()
